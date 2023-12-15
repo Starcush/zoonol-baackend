d8224e9e7a9693cbd37fd3aabc5e8feef134315a
sonhyoil <yhug76@jnjt.co.kr>
2023-12-15 14:38:51 +0900

    주소 검증 백업용 커밋

--- src/router/store.js
+++ src/router/store.js
@@ -3,6 +3,7 @@ import Router from '@koa/router';
 import StoreDomain from '@/domain/store';
 import db from '@/database';
 import { validateCtx } from '@/common/validator';
+import axios from 'axios';
 
 const storeRouter = new Router();
 const storeDomain = new StoreDomain({ db });
@@ -103,6 +104,95 @@ storeRouter.post('/update-store', async (ctx) => {
   const stores = await storeDomain.updateStore(validation);
   ctx.body = {
     stores,
+  };
+});
+storeRouter.get('/address-vertification', async (ctx) => {
+  const stores = await storeDomain.findStoreList();
+
+  const alivedStores = [];
+  const removedStores = [];
+
+  const Naver_Developer_Client_Id = '1jQd_VSHZdJNoy0mFIrf';
+  const Naver_Developer_Client_Secret_Id = '1BwSO2Ou6A';
+  const Naver_Search_Local_URL = 'https://openapi.naver.com/v1/search/local.json';
+
+  // 너무 빠른 request 요청을 방지하기 위한 딜레이 타이머
+  const delayTimer = ms => new Promise(res => setTimeout(res, ms))
+
+  // for(let i=0; i<stores.length; i++){
+  for(let i=0; i<100; i++){
+    console.log("SHI ::::: ===================================");
+    console.log("SHI :::: Index ::: ", i);
+    await delayTimer(1000);
+    
+    let store = stores[i];
+    let storeName = store.name;
+    let storeAddress = store.address;
+    let storeRoadAddress = store.roadAddress;
+    /**
+     * @breif data 형태
+     *  {
+            "title": "<b>닭향</b>",
+            "link": "",
+            "category": "한식>백숙,삼계탕",
+            "description": "",
+            "telephone": "",
+            "address": "경기도 김포시 대곶면 대벽리 537-4",
+            "roadAddress": "경기도 김포시 대곶면 수남로21번길 24-13",
+            "mapx": "1265887897",
+            "mapy": "376281206"
+        }
+     */
+    let { data } = await axios.get(
+      Naver_Search_Local_URL,
+      {
+        params: {
+          query: storeName,
+          display: 5
+        },
+        headers: {
+          'X-Naver-Client-Id': Naver_Developer_Client_Id,
+          'X-Naver-Client-Secret': Naver_Developer_Client_Secret_Id
+        }
+      }
+    );
+    
+    let naverStores = data.items;
+    if(naverStores.length > 0){
+      let naverStore = naverStores[0];
+      let naverStoreAddress = naverStore.address;
+      let naverStoreRoadAddress = naverStore.roadAddress;
+  
+      // if(storeAddress == naverStoreAddress && storeRoadAddress == naverStoreRoadAddress){
+      if(storeAddress == naverStoreAddress || storeRoadAddress == naverStoreRoadAddress){
+        // 주소값은 다르지만 도로명 주소만 같은 경우가 있어 && 에서 || 로 수정.
+        // 어떤 주소값이든 동일하면 남아있는 스토어로 테스트
+        alivedStores.push(store);
+      }
+      else{
+        // console.log("SHI :::: ============================================================================== ");
+        // console.log("SHI storeName :::: ", storeName);
+        // console.log("SHI storeAddress :::: ", storeAddress);
+        // console.log("SHI naverStoreAddress :::: ", naverStoreAddress);
+        // console.log("SHI storeRoadAddress :::: ", storeRoadAddress);
+        // console.log("SHI naverStoreRoadAddress :::: ", naverStoreRoadAddress);
+        // console.log("SHI :::: ============================================================================== ");
+        removedStores.push(store);
+      }
+    }
+    else{
+      // console.log("SHI :::: ******************************************************************************** ");
+      // console.log("SHI none data from naver open api :::: ");
+      // console.log("SHI storeName :::: ", storeName);
+      // console.log("SHI :::: ******************************************************************************** ");
+      removedStores.push(store);
+    }
+  }
+  
+  ctx.body = {
+    stores,
+    alivedStores,
+    removedStores
   };
 });
 
