1af1db978379a73d76336d08d1d6c9064eeac230
starcush <hacker0419@gmail.com>
2024-02-16 01:18:14 +0900

    자산배분 스냅샷 스케줄링 로직 추가

---
+++ src/asset_allocations/index.js
@@ -0,0 +1,162 @@
+import { google } from 'googleapis';
+import dayjs from 'dayjs';
+
+const sheets = google.sheets('v4');
+const credentials = require(`${process.env.PWD}/resources/zoonol-google-spreadsheet-google-service.json`);
+
+async function authenticate() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: credentials.client_email,
+      private_key: credentials.private_key,
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+
+  const authClient = await auth.getClient();
+  google.options({ auth: authClient });
+}
+
+async function getSheetRows(spreadsheetId, sheetName) {
+  const ALL_RANGE = 'A1:ZZ';
+
+  const sheets = google.sheets('v4');
+  const { data } = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: `${sheetName}!${ALL_RANGE}`,
+  });
+
+  return data.values;
+}
+
+async function searchValueFromRows(rows, target) {
+  const { rowName, columnName } = target;
+  let totalSumRow = -1;
+  let assetSumColumn = -1;
+
+  rows.forEach((row, rowIndex) => {
+    if (row.includes(rowName)) {
+      totalSumRow = rowIndex;
+    }
+  });
+
+  for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    const row = rows[rowIndex];
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      if (row[columnIndex] === columnName) {
+        assetSumColumn = columnIndex;
+        break;
+      }
+    }
+    if (assetSumColumn !== -1) {
+      break;
+    }
+  }
+
+  if (totalSumRow !== -1 && assetSumColumn !== -1) {
+    const value = rows[totalSumRow][assetSumColumn];
+
+    return { label: target.label, value };
+  } else {
+    console.log('총 합계 또는 자산별 합계를 찾을 수 없습니다.');
+    return null;
+  }
+}
+
+async function searchValuesFromRows(rows, targets) {
+  const results = [];
+
+  for (const target of targets) {
+    const result = await searchValueFromRows(rows, target);
+    results.push(result);
+  }
+
+  return results;
+}
+
+async function getWeeklyIndividualAssetsSnapshot(spreadsheetId) {
+  console.log('개별 자산 데이터 검색중...');
+
+  const rows = await getSheetRows(spreadsheetId, '개별자산');
+  const targets = [
+    { label: '평가금액', rowName: '총 합계', columnName: '자산별 합계' },
+    { label: '안전자산', rowName: '안전자산', columnName: '자산별 비율' },
+    { label: '배당자산', rowName: '배당자산', columnName: '자산별 비율' },
+    { label: '투자자산', rowName: '투자자산', columnName: '자산별 비율' },
+  ];
+
+  return await searchValuesFromRows(rows, targets);
+}
+
+async function getInitialInvestment(spreadsheetId) {
+  const rows = await getSheetRows(spreadsheetId, '투자일지');
+  const target = { label: '투자원금', rowName: '투자원금', columnName: '합계' };
+
+  return await searchValueFromRows(rows, target);
+}
+
+async function insertIndividualAssets({ spreadsheetId, individualAssets, initialInvestment }) {
+  const rows = await getSheetRows(spreadsheetId, '분석(2024)');
+  const today = dayjs().format('YYYY/MM/DD');
+
+  const insertValues = [...individualAssets, initialInvestment, { label: '날짜', value: today }];
+
+  const insertToSheet = async (label, value) => {
+    let targetCell;
+
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i].includes(label)) {
+        let lastNonEmptyCellIndex = -1;
+        for (let j = 0; j < rows[i].length; j++) {
+          if (rows[i][j]) {
+            lastNonEmptyCellIndex = j;
+          }
+        }
+
+        if (lastNonEmptyCellIndex !== -1) {
+          const nextCellIndex = lastNonEmptyCellIndex + 1;
+          const columnLetter = String.fromCharCode('A'.charCodeAt(0) + nextCellIndex);
+          targetCell = `분석(2024)!${columnLetter}${i + 1}`;
+          break;
+        }
+      }
+    }
+
+    if (targetCell) {
+      console.log(`값을 입력할 셀: ${targetCell}`);
+    } else {
+      console.log('평가금액 행을 찾을 수 없거나, 적절한 셀을 찾을 수 없습니다.');
+    }
+
+    if (targetCell) {
+      await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range: targetCell,
+        valueInputOption: 'USER_ENTERED',
+        requestBody: {
+          values: [[value]],
+        },
+      });
+      console.log(`Value ${value} written to ${targetCell}`);
+    } else {
+      console.log('No empty cell found.');
+    }
+  };
+
+  for (const insertValue of insertValues) {
+    const { label, value } = insertValue;
+    await insertToSheet(label, value);
+  }
+}
+
+async function weeklyAssetAllocation() {
+  await authenticate();
+  console.log('----------------------------');
+  const spreadsheetId = '1Gp-HspzJ969rwImrNhLNyiNf2dceBTs4qtDeQ8ZUvmA';
+
+  const individualAssets = await getWeeklyIndividualAssetsSnapshot(spreadsheetId);
+  const initialInvestment = await getInitialInvestment(spreadsheetId);
+  await insertIndividualAssets({ spreadsheetId, individualAssets, initialInvestment });
+}
+
+export default weeklyAssetAllocation;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import './dotenv';
 import Koa from 'koa';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
+import cron from 'node-cron';
 import router from '@/router/index';
+import weeklyAssetAllocation from './asset_allocations';
+
+cron.schedule('0 9 * * 0', () => {
+  console.log('스케줄링 작업이 실행됩니다.');
+  weeklyAssetAllocation().catch((error) => console.log(error));
+});
 
 const { SERVER_PORT } = process.env;
 
